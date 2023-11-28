c504ab95622fac531408463aa7411ecee8f52325
mwasim <mwasimse@gmail.com>
2023-11-28 11:45:28 +0500

    JavascriptEssentials: Practiced - created new Javascript Object (mobile phone) and rendered output in HTML

--- javascript-essential-training/Practice/03_07/script.js
+++ javascript-essential-training/Practice/03_07/script.js
@@ -7,3 +7,38 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const mobilePhone = {
+  name: "OPPO Mobile",
+  color: "white",
+  OS: "Android",
+  resolution: {
+    width: 1080,
+    height: 1920,
+  },
+  features: {
+    supportsBluetooth: true,
+    supportsWifi: true,
+  },
+};
+
+const markup = (mobilePhone) => {
+  return `
+        <div>
+            <h1>${mobilePhone.name}</h1>
+            <ul>
+                <li><strong>Color: </strong>${mobilePhone.color}</li>
+                <li><strong>OS: </strong>${mobilePhone.OS}</li>
+            </ul>
+            <h2>Resolution</h2>
+            <ul>
+                <li><strong>Width: </strong>${mobilePhone.resolution.width}</li>
+                <li><strong>Heiht: </strong>${mobilePhone.resolution.height}</li>
+            </ul>
+        </div>
+    `;
+};
+
+const main = document.createElement("main");
+main.innerHTML = markup(mobilePhone);
+document.body.appendChild(main);

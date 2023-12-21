65c4c6f042db407a2932bb2ac2a6bb475690482b
mwasim <mwasimse@gmail.com>
2023-12-21 14:16:08 +0500

    JavaScriptEssentials: Array.map method demo

--- javascript-essential-training/08_15/script.js
+++ javascript-essential-training/08_15/script.js
@@ -3,16 +3,30 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
  */
 
-const stuff = ["piggy", "headlamp", "pen", "pencil", "eraser", "water bottle"];
+const stuff = ["catty", "headlamp", "pen", "pencil", "eraser", "water bottle"];
 
 const article = document.querySelector("article");
 let stuffList = document.createElement("ul");
 
 // forEach() array method
+/*
 stuff.forEach((item) => {
   let listItem = document.createElement("li");
   listItem.innerHTML = item;
   stuffList.append(listItem);
+});*/
+
+var stuffItems = stuff.map((item) => {
+  let listItem = document.createElement("li");
+  listItem.innerHTML = item;
+  return listItem;
 });
 
-article.append(stuffList)
+console.log("stuff", stuff);
+console.log("stuffItems", stuffItems);
+
+stuffItems.forEach((listElement) => {
+  stuffList.append(listElement);
+});
+
+article.append(stuffList);

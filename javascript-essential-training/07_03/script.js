c5c4dd3f9e9b3b1d81741364045e41ebf085ae58
mwasim <mwasimse@gmail.com>
2023-12-21 11:41:16 +0500

    JavaScriptEssentialTraining: Array methods demo

--- javascript-essential-training/07_03/script.js
+++ javascript-essential-training/07_03/script.js
@@ -3,9 +3,27 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array#Instance_methods
  */
 
-let backpackContents = ["piggy", "headlamp", "pen"];
+let backpackContents = ["calculator", "headlamp", "pen"];
 
-console.log(backpackContents);
+backpackContents.forEach((item, index) => {
+  console.log(`<li>${item} - ${index}</li>`);
+});
+
+backpackContents.push("pencil", "razer", 5);
+//backpackContents.shift(); //takes the first item off the array
+//backpackContents.pop(); //removes the last element from the array and return it.
+backpackContents.unshift("pencil");
+
+let longItems = backpackContents.find((item) => item.length >= 5); //finds single item
+
+//The above line of code can also be written as below,
+let longItems1 = backpackContents.find(function (item) {
+  if (item.length >= 8) return item;
+});
+
+console.log(backpackContents.join(", "));
+console.log("Long Items:" + longItems);
+console.log("Long Items:" + longItems1);
 
 // backpackContents.forEach(function (item) {
 //   item = `<li>${item}</li>`;

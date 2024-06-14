e457a0824b6ee87046c77f6e7c76b4ce319716c4
Wasim <mwasimse@gmail.com>
2024-06-14 19:00:15 +0500

    FCC: Algorithmic Thinking by Building a Dice game - Update Stats and Options functions implemented.

--- fcc-dice-game/script.js
+++ fcc-dice-game/script.js
@@ -30,12 +30,26 @@ const rollDice = () => {
   });
 };
 
+const updateStats = () => {
+  currentRoundRollsText.textContent = rolls;
+  currentRoundText.textContent = round;
+};
+
+const updateRadioOption = (optionNode, score) => {
+  scoreInputs[optionNode].disabled = false;
+  scoreInputs[optionNode].value = score;
+  scoreSpans[optionNode].textContent = `, score = ${score}`;
+};
+
+const getHighestDuplicates = (arr) => {};
+
 rollDiceBtn.addEventListener("click", () => {
   if (rolls === 3) {
     alert("You have made three rolls this round. Please select a score.");
   } else {
     rolls++;
     rollDice();
+    updateStats();
   }
 });
 

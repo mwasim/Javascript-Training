b79c52036bd8ce1851509809625c2f1cc921141b
Wasim <mwasimse@gmail.com>
2024-06-24 15:15:03 +0500

    FCC - Building Authors page using Fetch and Promises - implemented displayAuthors to render authors cards.

--- fcc-fetch-promises-authors/script.js
+++ fcc-fetch-promises-authors/script.js
@@ -1,19 +1,29 @@
-const authorContainer = document.getElementById("author-container");
-const loadMoreBtn = document.getElementById("load-more-btn");
+const authorContainer = document.getElementById('author-container');
+const loadMoreBtn = document.getElementById('load-more-btn');
 
 let startingIndex = 0;
 let endingIndex = 8;
 let authorDataArr = [];
 
-fetch("https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json")
+fetch('https://cdn.freecodecamp.org/curriculum/news-author-page/authors.json')
   .then((res) => res.json())
   .then((data) => {
-    console.log(data);
+    authorDataArr = data;
+    displayAuthors(authorDataArr.slice(startingIndex, endingIndex));   
   })
   .catch((err) => {
     console.error(`There was an error: ${err}`);
   });
 
 const displayAuthors = (authors) => {
-  authors.forEach(({ author, image, url, bio }, index) => {});
+  authors.forEach( ({ author, image, url, bio }, index) =>
+  (authorContainer.innerHTML += `
+    <div id="${index}" class="user-card">
+      <h2 class="author-name">${author}</h2>
+      <img class="user-img" src="${image}" alt="${author} avatar" />
+      <p class="bio">${bio}</p>
+<a href="${url}" target='_blank' class='author-link'>${author}'s page</a>
+    </div>
+  `)
+  )
 };

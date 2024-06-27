dbf61195bf4fb0ce009a69034eaa045ae22ff187
Wasim <mwasimse@gmail.com>
2024-06-27 09:26:18 +0500

    FCC - Asynchronous Programming by building forum leaderboard: show latest posts feature implemented.

--- fcc-forum-leaderboard/script.js
+++ fcc-forum-leaderboard/script.js
@@ -1,17 +1,49 @@
-const forumLatest =
-  "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
+const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
 const forumTopicUrl = "https://forum.freecodecamp.org/t/";
 const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
 const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
 
+
+
 const fetchData = async () => {
   try {
     const res = await fetch(forumLatest);
     const data = await res.json();
-    console.log(data);
-  } catch (err) {}
+    showLatestPosts(data);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 fetchData();
+
+const showLatestPosts = (data) => {
+  const { topic_list, users } = data;
+  const { topics } = topic_list;
+
+  postsContainer.innerHTML = topics.map((item) => {
+    const {
+      id,
+      title,
+      views,
+      posts_count,
+      slug,
+      posters,_id,
+      bumped_at,
+    } = item;
+
+    return `
+    <tr>
+      <td>
+        <p class="post-title">${title}</p>
+      </td>
+      <td></td>
+      <td>${posts_count - 1}</td>
+      <td>${views}</td>
+      <td></td>
+    </tr>`;
+  }).join("");
+};
+

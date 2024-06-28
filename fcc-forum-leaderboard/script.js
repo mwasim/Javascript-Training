2cdb3811e8b408c6be60e4fe2aabcdc0ebf75429
Wasim <mwasimse@gmail.com>
2024-06-28 09:29:06 +0500

    FCC - Asynchronous Programming by building forum leaderboard: Formatted views count and activity time in readable way.

--- fcc-forum-leaderboard/script.js
+++ fcc-forum-leaderboard/script.js
@@ -1,11 +1,42 @@
-const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
+const forumLatest =
+  "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
 const forumTopicUrl = "https://forum.freecodecamp.org/t/";
 const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
 const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
 const postsContainer = document.getElementById("posts-container");
 
+const timeAgo = (time) => {
+  const currentTime = new Date();
+  const lastPost = new Date(time);
 
+  const timeDifference = currentTime - lastPost;
+  const msPerMinute = 1000 * 60;
+
+  const minutesAgo = Math.floor(timeDifference / msPerMinute);
+  const hoursAgo = Math.floor(minutesAgo / 60);
+  const daysAgo = Math.floor(hoursAgo / 24);
+
+  if (minutesAgo < 60) {
+    return `${minutesAgo}m ago`;
+  }
+
+  if (hoursAgo < 24) {
+    return `${hoursAgo}h ago`;
+  }
+
+  return `${daysAgo}d ago`;
+};
+
+const viewCount = (views) => {
+  const thousands = Math.floor(views / 1000);
+
+  if (views >= 1000) {
+    return `${thousands}k`;
+  }
+
+  return views;
+};
 
 const fetchData = async () => {
   try {
@@ -23,27 +54,29 @@ const showLatestPosts = (data) => {
   const { topic_list, users } = data;
   const { topics } = topic_list;
 
-  postsContainer.innerHTML = topics.map((item) => {
-    const {
-      id,
-      title,
-      views,
-      posts_count,
-      slug,
-      posters,_id,
-      bumped_at,
-    } = item;
+  postsContainer.innerHTML = topics
+    .map((item) => {
+      const {
+        id,
+        title,
+        views,
+        posts_count,
+        slug,
+        posters,
+        category_id,
+        bumped_at,
+      } = item;
 
-    return `
+      return `
     <tr>
       <td>
         <p class="post-title">${title}</p>
       </td>
       <td></td>
       <td>${posts_count - 1}</td>
-      <td>${views}</td>
-      <td></td>
+      <td>${viewCount(views)}</td>
+      <td>${timeAgo(bumped_at)}</td>
     </tr>`;
-  }).join("");
+    })
+    .join("");
 };
-

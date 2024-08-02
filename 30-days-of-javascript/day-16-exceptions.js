39edda9774f8fa29dbdbf8f8a112cbc11ef72384
Wasim <mwasimse@gmail.com>
2024-08-03 04:51:50 +0500

    30 Days of JavaScript - Day 16: Exception handling challenge challenge completed.

---
+++ 30-days-of-javascript/day-16-exceptions.js
@@ -0,0 +1,20 @@
+/*
+Task
+Read a string, S, and print its integer value; if S cannot be converted to an integer, print Bad String.
+Note: You must use the String-to-Integer and exception handling constructs built into your submission language. If you attempt to use loops/conditional statements, you will get a 0 score.
+*/
+
+function main(input) {
+  const S = input;
+
+  try {
+    const result = parseInt(S, 10);
+    if (isNaN(result)) throw new Error("Bad String");
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+main(3);
+main("za");

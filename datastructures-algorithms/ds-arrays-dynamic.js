0bab479bc4d2501010635e78dcafb0eb66dadffd
Wasim <mwasimse@gmail.com>
2024-08-20 13:52:43 +0500

    DSA - Dynamic Arrays challenge completed. Solved given problem. Given problem description added in comments.

--- datastructures-algorithms/ds-arrays-dynamic.js
+++ datastructures-algorithms/ds-arrays-dynamic.js
@@ -1,4 +1,47 @@
 /*
+    Given Task/Problem,
+
+    Declare a 2-dimensional array, arr, of n empty arrays. All arrays are zero indexed.
+    Declare an integer, lastAnswer, and initialize it to 0.
+
+    There are 2 types of queries, given as an array of strings for you to parse:
+
+    Query: 1 x y
+    Let idx = ((x XOR lastAnswer)%n).
+    Append the integer y to arr[idx].
+
+    Query: 2 x y
+    Let idx = ((x XOR lastAnswer)%n).
+    Assign the value arr[idx]y%size(arr[idx]) to lastAnswer.
+    Store the new value of lastAnswer to an answers array.
+
+    Note:  is the bitwise XOR operation, which corresponds to the ^ operator in most languages.
+    Finally, size(arr[idx]) is the number of elements in arr[idx]
+
+    Function Description
+    Complete the dynamicArray function below.
+
+    dynamicArray has the following parameters:
+    - int n: the number of empty arrays to initialize in arr
+    - string queries[q]: query strings that contain 3 space-separated integers
+
+    Returns
+    int[]: the results of each type 2 query in the order they are presented
+
+    Sample Input
+    2 5
+    1 0 5
+    1 1 7
+    1 0 3
+    2 1 0
+    2 1 1
+
+    Sample Output
+    7
+    3
+*/
+
+/*
  * Complete the 'dynamicArray' function below.
  *
  * The function is expected to return an INTEGER_ARRAY.

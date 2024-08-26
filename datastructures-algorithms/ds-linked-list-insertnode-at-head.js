2f95878ed7da2321aa4d00ed928a2ebdb95dc824
Wasim <mwasimse@gmail.com>
2024-08-26 16:32:11 +0500

    DSA - Linked Lists - Insert Singly Linked List Node challenge completed.

---
+++ datastructures-algorithms/ds-linked-list-insertnode-at-head.js
@@ -0,0 +1,65 @@
+const SinglyLinkedListNode = class {
+  constructor(nodeData) {
+    this.data = nodeData;
+    this.next = null;
+  }
+};
+
+const SinglyLinkedList = class {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+};
+
+function printSinglyLinkedList(node, sep, ws) {
+  while (node != null) {
+    ws.write(String(node.data));
+
+    node = node.next;
+
+    if (node != null) {
+      ws.write(sep);
+    }
+  }
+}
+
+// Complete the insertNodeAtHead function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function insertNodeAtHead(head, data) {
+  const newNode = new SinglyLinkedListNode(data);
+
+  if (!head) return newNode;
+
+  newNode.next = head;
+
+  return newNode;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const llistCount = parseInt(readLine(), 10);
+
+  let llist = new SinglyLinkedList();
+
+  for (let i = 0; i < llistCount; i++) {
+    const llistItem = parseInt(readLine(), 10);
+    const llist_head = insertNodeAtHead(llist.head, llistItem);
+    llist.head = llist_head;
+  }
+
+  printSinglyLinkedList(llist.head, "\n", ws);
+  ws.write("\n");
+
+  ws.end();
+}
